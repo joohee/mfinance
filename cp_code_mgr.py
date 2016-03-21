c8ea928af9814a49ce39e5f20f448eac4fa52f9e
Joey <neigie@gmail.com>
2016-03-21 11:00:40 +0900

    degbug

--- cp_code_mgr.py
+++ cp_code_mgr.py
@@ -10,12 +10,12 @@ def cp_code_mgr():
     today = datetime.datetime.now()
     with open(today.strftime('%Y%m%d')+'_cpCodeMgr_detail_all.csv', 'w', encoding='utf-8') as f:
         header_list = []
-        for i in len(codes.header_dic):
+        for i in range(len(codes.header_dic)):
             header_list.append(codes.get_header(str(i)))
         f.write('\t'.join(header_list))
         f.write('\n')
 
-        for idx in len(stock_market_code_dic):
+        for idx in range(len(codes.stock_market_code_dic)):
             codeList = instCpCodeMgr.GetStockListByMarket(idx)
             
             str_list = []
@@ -35,7 +35,7 @@ def cp_code_mgr():
                 str_list.append(str(i))
                 str_list.append(code)
                 str_list.append(name)
-                str_list.append(codes.get_code_dic(str(secondCode)))
+                str_list.append(codes.get_stock_code(str(secondCode)))
                 str_list.append(codes.get_stock_market_kind(str(marketKindCode)))
                 str_list.append(codes.get_stock_kospi_200_kind(str(kospi200Code))+"("+str(kospi200Code)+")")
                 str_list.append(industryName+"("+str(industryCode)+")")
