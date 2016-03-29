9e248bff9d25cb3a4ea013e8a8cdd21c8b237aa8
Joohee Kang <neigie@gmail.com>
2016-03-29 16:43:54 +0900

    use multi-thread

--- threads/cp_stock_chart_thread.py
+++ threads/cp_stock_chart_thread.py
@@ -1,7 +1,8 @@
 import threading
 from queue import Queue
 import time
-from cp_stock_chart import cp_stock_chart
+import datetime
+from packages.cp_stock_chart import StockChart
 
 class CpThread:
 
@@ -9,22 +10,29 @@ class CpThread:
         self.print_lock = threading.Lock()
         self.q = Queue()
 
-    def exampleJob(self, worker):
+    def convert_date(date):
+        return date.strftime('%Y%m%d')
+
+    # 실제 작업하는 놈
+    def stockChart(self, worker, from_date, to_date):
         time.sleep(0.5)
 
         with self.print_lock:
-            print("name: {}, worker: {}".format(threading.current_thread().name, worker))
+            print("name: {}, worker: {}, from_date: {}, to_date: {}".format(threading.current_thread().name, worker, from_date, to_date))
+            stockChart = StockChart('A067160', convert_date(from_date), convert_date(to_date))
 
-    def threader(self):
+    def threader(self, from_date, to_date):
         while True:
             worker = self.q.get()
-            self.exampleJob(worker)
+            self.stockChart(worker, from_date, to_date)
             self.q.task_done()
 
     def work(self):
+        from_date = datetime.datetime.now() 
 
         for x in range(10):
-            t = threading.Thread(target = self.threader)
+            to_date = from_date - datetime.timedelta(days=x)
+            t = threading.Thread(target = self.threader, args=[from_date, to_date])
             t.daemon = True
             t.start()
 
