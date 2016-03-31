d68b15441fc6b21f896997cf8fbe1659204d6560
Joohee Kang <neigie@gmail.com>
2016-03-31 11:07:54 +0900

    docstring added

--- cyboplus/cp_code_mgr_industry_code.py
+++ cyboplus/cp_code_mgr_industry_code.py
@@ -2,6 +2,20 @@ import win32com.client
 import datetime
 
 def cp_code_mgr():
+    """ Market 별 소속된 종목 코드 및 종목 명을 print 한다. 
+
+        Args: 
+            없습니다.
+
+        Returns:
+            code 명 - 산업명 
+            code - 산업코드 
+            groupName : 그룹명(계열사명)
+            groupCode : 그룹코드 
+            ex. 
+                code: 대형(시가총액)(002), groupCode: BGF리테일(A027410)
+
+    """
     instCpCodeMgr = win32com.client.Dispatch("CpUtil.CpCodeMgr")
     codeList = instCpCodeMgr.GetStockListByMarket(1)
     today = datetime.datetime.now()
